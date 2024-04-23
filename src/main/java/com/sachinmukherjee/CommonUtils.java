291d4fb9610505f7a4f99d8541e07689b2a35416
sachinmukherjee <sachinmukherjee29@gmail.com>
2024-04-23 21:54:02 +0530

    created basic classes for tree traversal

---
+++ src/main/java/com/sachinmukherjee/CommonUtils.java
@@ -0,0 +1,21 @@
+package com.sachinmukherjee;
+
+import com.sachinmukherjee.tree.binary.Node;
+
+public class CommonUtils {
+	
+	private CommonUtils() {
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	public static void insertData(Node root) {
+		
+	}
+	
+	
+	public static void printTree(Node root) {
+		
+	}
+
+}
---
+++ src/main/java/com/sachinmukherjee/tree/binary/tree/traversal/recursive/PreOrderTraversal.java
@@ -0,0 +1,5 @@
+package com.sachinmukherjee.tree.binary.tree.traversal.recursive;
+
+public class PreOrderTraversal {
+
+}
